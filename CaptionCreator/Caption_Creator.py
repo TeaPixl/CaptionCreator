d7c336e569947327dd023e09375f166852a84e21
TeaPixl <106362493+TeaPixl@users.noreply.github.com>
2022-05-26 22:55:25 -0400

    Add files via upload

---
+++ CaptionCreator/Caption_Creator.py
@@ -0,0 +1,76 @@
+# ********************---THE PROGRAM---********************
+
+import speech_recognition as sr
+import progressbar
+import time
+import os
+
+# greeting into the program
+print("**********Caption Creator**********\n")
+print("This only works for .wav, .mp3, .mp4, and .mkv file formats")
+print("Place your file in the folder labeled Audio before you start")
+answer = input("Would you like to start the Creator? (y/n): ")
+if answer == "y":
+    pass
+else:
+    print("Goodbye!")
+    time.sleep(2)
+    exit()
+
+# check for caption files that are already there and if so, delete them
+try:
+    os.remove('Captions\\Captions.txt')
+except FileNotFoundError:
+    print("No captions to remove...")
+    pass
+
+# animated progressbar
+def animated_progressbar():
+    widgets = ['Loading: ', progressbar.AnimatedMarker()]
+    bar = progressbar.ProgressBar(widgets=widgets).start()
+    
+    for i in range(50):
+        time.sleep(0.1)
+        bar.update(i)
+        
+recognise = sr.Recognizer()
+
+# accesses the file
+try:
+    import mp3_converter
+    import mp4_converter
+    import mkv_converter
+    audio_file = sr.AudioFile("Audio\\audio.wav")  # tries to access a .wav file
+except Exception:
+    print("Error was caught, check for unsupported file types or if there is no file") # any extra errors caught
+    time.sleep(5)
+finally: 
+    
+    # records the speech in the file
+    with audio_file as source:
+        recognise.adjust_for_ambient_noise(source)
+        print("Adjusted recording for background noise cancellation...")
+        # shows an animated progress bar
+        animated_progressbar()
+        # records and sends speech to API
+        audio = recognise.record(source)
+        # I am using the Sphinx API, feel free to change this if you would like
+        finished_audio = recognise.recognize_google(audio)
+        # begins to write captions to a file
+        # shows an animated progress bar
+        animated_progressbar()
+        with open('Captions\\Captions.txt', "w") as f:
+            print("Writing to a file...")
+            f.write("***CAPTIONS***\n\n")
+            f.write(finished_audio.capitalize())
+            f.write("\n\n")
+            f.write("-Credits go to L.T, Thx :D") # credits (of course :/)
+            f.close
+            
+os.remove("Audio\\audio.wav") # removes the original file
+print("Removing audio.wav...")
+print("Done.")
+print("Text file has finished processing")
+print("Check the Captions folder")
+time.sleep(10) # extra time to show the finished text
+exit()
---
+++ CaptionCreator/README.md
@@ -0,0 +1,24 @@
+Caption creator that uses natural language processing from a speech-to-text API to process and create accurate captions
+simply drag and drop the .wav, .mp4, .mkv, or .mp3 files into the program folder as audio and it will output your caption .txt file on the desktop
+This program only supports .mkv, .wav, .mp3 and .mp4 file formats
+DO NOT STOP THE PROGRAM IF IT STOPS LOADING, this is completly normal, longer audio files will take some time
+YOUR ORIGINAL AUDIO OR VIDEO FILE WILL BE LOST IN THE PROCESS SO BE SURE TO HAVE A BACKUP
+Click the .exe file to start the program
+
+I am not held liable for any loss of video or audio from misuse of this software, Thanks!
+Creation and Credits go to L.T :)
+
+
+
+"""Documentation""" 
+
+There are many files in this folder that will be used.
+You simply want to paste you files into the directory labeled "Audio" to convert the file, it will automatically delete the original audio file after completion
+MAKE SURE TO RENAME THE FILE TO "audio"
+The "speech_process.py" file is the main file used in the program and controls all other files in the directory
+The "mp3_converter.py", "mp4_converter.py" and "mkv_converter" are used to convert a mp3 or mp4 file into a wav format that is used by the speech analysis program unless the file is already in a wav format.
+The "README.md" file is what you are looking at now! and contains all of the documentation for the program
+The "Captions" directory stores all of your finished captions from your audio files,
+You can take these files and store them anywhere to be used, or leave them and they will automatically be deleted the next time the program boots
+The "source.py" file is the source code for this program and the brain of it, it can be used to develop your own additions
+You can change the API that is used to convert speech into text with the recognise_[] function
